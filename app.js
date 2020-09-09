0f72b0bf78650bac165d7ffe179a0ea7ef79b8d3
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-09-09 17:01:05 +0100

    added filtered tasks

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
-  //Remove task event
+  // Remove task event
   taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -24,36 +28,56 @@ function addTask(e) {
 
   // Create li element
   const li = document.createElement('li');
-  
   // Add class
-  li.className - 'collection-item';
-  //create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value))
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
   const link = document.createElement('a');
   // Add class
   link.className = 'delete-item secondary-content';
   // Add icon html
   link.innerHTML = '<i class="fa fa-remove"></i>';
-  //Append the link to li
+  // Append the link to li
   li.appendChild(link);
 
   // Append li to ul
   taskList.appendChild(li);
 
-  // Cleat input
+  // Clear input
   taskInput.value = '';
-
 
   e.preventDefault();
 }
 
 // Remove Task
-
 function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')) {
-    if(confirm('Are you sure, this operation cannot be undone')) {
+    if(confirm('Are You Sure?')) {
       e.target.parentElement.parentElement.remove();
     }
   }
+}
+
+// Clear Tasks
+function clearTasks() {
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
